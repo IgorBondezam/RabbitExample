e252bae442997e2196e36d6c7bcb3b277901695b
Igor Bondezam <91677774+IgorBondezam@users.noreply.github.com>
2024-10-24 20:35:32 -0300

    Criando RabbitMQ produtores e consumidores

---
+++ RabbitMQ/src/main/java/br/com/unicesumar/igor/RabbitMQ/RabbitMqApplication.java
@@ -0,0 +1,22 @@
+package br.com.unicesumar.igor.RabbitMQ;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.DeliverCallback;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.IOException;
+import java.sql.Array;
+
+@SpringBootApplication
+public class RabbitMqApplication {
+
+	private final static String QUEUE_NAME = "hello";
+
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(RabbitMqApplication.class, args);
+	}
+}
